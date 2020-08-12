ddff4c34fe3c4a019f4f2cfcbdde95b423a8c7bf
EstherChenn <46521202+EstherChenn@users.noreply.github.com>
2020-08-13 07:28:49 +0800

    第三章 栈和队列

--- C-Algorithm/Queue.c
+++ C-Algorithm/Queue.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <malloc.h>
 
 #define maxsize 5
 
@@ -20,6 +21,103 @@ typedef struct LiQueue {
 	QNode* front;	//队头指针
 	QNode* rear;	//队尾指针
 }LiQueue;
+
+//初始化队列
+void SqQueue_init(SqQueue *sq) {
+	sq->front = 0;
+	sq->rear = 0;
+}
+
+//判断队空算法
+int SqQueue_isEmpty(SqQueue* sq) {
+	if (sq->rear == sq->front) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+//进队算法
+int SqQueue_en(SqQueue** sq, int x) {
+	//判断是否队满
+	if ((*sq)->front == (*sq)->rear) {
+		return 0;
+	}
+	(*sq)->rear = ((*sq)->rear + 1) % maxsize;
+	(*sq)->data[(*sq)->rear] = x;
+	return 1;
+}
+
+//出队算法
+int SqQueue_out(SqQueue** sq) {
+	int x;
+	//判断是否队空
+	if ((*sq)->front == (*sq)->rear)
+		return 0;
+	(*sq)->front = ((*sq)->front + 1) % maxsize;
+	x = (*sq)->data[(*sq)->front];
+	return 1;
+}
+ 
+//初始化链队
+void LiQueue_init(LiQueue** lqu) {
+	*lqu = (LiQueue*)malloc(sizeof(LiQueue));
+	(*lqu)->front = NULL;
+	(*lqu)->rear = NULL;
+}
+
+//判断队空算法
+int LiQueue_isEmpty(LiQueue* lqu) {
+	if ((lqu->rear == NULL) || (lqu->front == NULL)) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+//链队入队算法
+int LiQueue_en(LiQueue** lqu, int x) {
+	QNode* p;
+	p = (QNode*)malloc(sizeof(QNode));
+	p->data = x;
+	p->next = NULL;
+	//若队空，则新结点是队首结点，也是队尾结点
+	if ((*lqu)->rear == NULL) {
+		(*lqu)->front = p;
+		(*lqu)->rear = p;
+	}
+	else {
+		(*lqu)->rear->next = p;
+		(*lqu)->rear = p;
+	}
+}
+
+
+//链队出队算法
+int LiQueue_out(LiQueue** lqu) {
+	int x;
+	QNode* p;
+	//若队空
+	if ((*lqu)->rear == NULL) {
+		return 0;
+	}
+	else {
+		p = (*lqu)->front;
+	}
+	//队中只有一个结点时需特殊处理
+	if ((*lqu)->front == (*lqu)->rear) {
+		(*lqu)->front = NULL;
+		(*lqu)->rear = NULL;
+	}
+	else {
+		(*lqu)->front = (*lqu)->front->next;
+	}
+	x = p->data;
+	free(p);
+	return 1;
+}
 
 int main() {
 
--- C-Algorithm/Stack.c
+++ C-Algorithm/Stack.c
@@ -150,7 +150,7 @@ int op(int a, char Op, int b) {
 		return a * b;
 	if (Op == '/') {
 		if (b == 0)		//除数不为0
-			return 0;
+			return -1;
 		else
 			return a / b;
 	}
@@ -160,22 +160,67 @@ int com(char exp[]) {
 	char data[maxsize];
 	int top = -1;
 	int i=0;
-	int a, b;
+	int a, b, result;
 	while (exp[i] != '\0') {
 		//判断值为数字还是运算符
 		if (exp[i] >= '0' && exp[i] <= '9') {
 			data[++top] = exp[i];
 		}
 		else {
-			//判断是否栈空
-			if (top != -1) {
-				//将字符型数字转换为整型
+			//判断是否栈空或者栈里只有一个数字
+			if ((top == -1) || (top == 0)) {
+				printf("输入格式错误");
+				return 0;
 			}
+			//出栈
+			a = (int)data[top--];
+			b = (int)data[top--];
+			result = op(a, exp[i], b);
+			//如果输出正确,则结果入栈
+			if (result == -1)
+				return 0;
+			data[++top] = result;
 		}
 	}
+	return data[top];
 }
 
 //3-3 用不带头结点的单链表存储链栈，设计初始化栈,判断栈是否为空，进栈和出栈等相应的算法
+
+
+//2.6 假设以带头结点的循环链表表示队列，并且只设一个指针指向队尾结点，但不设头指针，请写出相应的入队列和出队列算法
+
+//2.7 如果允许在循环队列的两端都可以进行插入和删除操作，要求：
+//1） 写出循环队列的类型定义
+//2） 分别写出从队尾删除和从队头插入的算法
+
+
+//2.8 设计一个循环队列，用front 和rear分别作为队头和队尾指针，另外用一个标志tag表示队列是空还是不空，约定当tag为0时队空，当tag为1时队不空，当tag
+//为1时队不空，这样就可以用front==rear作为队满的条件要求，设计队列的结构和相关基本运算算法（队列元素为int型）
+
+//2.9 编写一个算法，将一个非负的十进制整数N转换为一个二进制数
+
+//2.10 试编写一个算法，检查一个程序中的花括号，方括号和愿括号是否配对，若全部配对，则返回1，否则返回0.对于程序中出现的一对单引号或双引号内的字符
+//不进行括号配对检查。39为单引号的ASCII值，34为双引号的ASCII值，单引号和双引号如果出现则必成对出现。
+//假设stack试已经定义的顺序栈结构体。可以直接调用的元素进栈/出栈，取栈顶元素，判断栈空的函数定义如下：
+void push(stack* S, char ch) {
+
+}
+void pop(stack* S, char ch) {
+
+}
+void getTop(stack S, char ch) {
+
+}
+int isEmpty(stack s) {
+
+}
+
+
+
+
+
+
 
 
 
