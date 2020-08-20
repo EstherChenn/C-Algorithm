19c957745a28322212b0218ee48a05151ac369b7
EstherChenn <46521202+EstherChenn@users.noreply.github.com>
2020-08-21 07:31:27 +0800

    c60-54

--- C-Algorithm/c60-1.c
+++ C-Algorithm/c60-1.c
@@ -3,6 +3,12 @@
 
 #define maxsize 100
 
+//定义栈的结构体
+typedef struct LNode {
+	int data;
+	struct LNode* next;
+}LNode;
+
 //1. 编写一个程序求三个整数的最大值
 int c1(int a,int b,int c) {
 	int max = a;
@@ -455,45 +461,405 @@ void c31() {
 	printf("\n");
 }
 
-//32. 逆序输出存储整数的数组
+//32. 逆序输出存储整数的数组(将一个从键盘输入的整数存于一个数组中，通过程序的运行按数组中的逆序输出该整数，利用递归的方法解决问题)
+int i = 0;
+int c32(int k) {
+	int a[maxsize] ;  
+	if ((a[k] = getchar()) == '\n') {
+		return 0;
+	}
+	else {
+		printf("%d\n", a[k]);
+		c32(k - 1);
+	}
+}
 
 //33. 将一个一维数组就地逆置
+void c33() {
+	int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k, i;
+	for (i = 0; i < 5; i++) {
+		k = a[i];
+		a[i] = a[10 - i];
+		a[10 - i] = k;
+	}
+	for (i = 0; i < 10; j++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n");
+}
 
 //34. 数据加密（四位的整数，每位数字都加5，然后用和除以10的余数代替该数字，再将第一位和第四位交换，第二位和第三位交换）
+void c34() {
+	int a;
+	int i, index;
+	printf("请输入 一个四位数的整数：\n");
+	scanf_s("%d", &a);
+	index = (a % 10 + 5) + (a / 10 % 10 + 5) + (a / 100 % 10 + 5) + (a / 1000 + 5);
+	index = index % 10;
+	index = (index % 10 * 1000) + (index / 1000) + (index / 10 % 10 * 100) + (index / 100 % 10 * 10);
+	printf("result : %d\n", index);
+}
 
 //35. 输出菱形
+void c35() {
+	int i, j, n;
+	printf("请输入菱形的层数：\n");
+	scanf_s("%d", &n);
+	int k = n / 2;
+	for (i = 0; i < n / 2; i++) {
+		for (j = 0; j < n; j++) {
+			if()
+		}
+	}
 
-//36. 求2-300范围内完数的个数
+}
 
-//37. 求孪生素数（相邻的两个素数）
+//36. 求2-300范围内完数的个数(这个数巧合等于它的因子之和，除了本身外)
+void c36() {
+	int n, i, sum;
+	printf("2-300范围内完数:\n");
+	for (n = 2; n <= 300; n++) {
+		sum = 0;
+		for (i = 1; i < n; i++) {
+			if (n % i == 0)
+				sum += i;
+		}
+		if (n == sum) {
+			printf("%d ", n);
+		}
+	}
+	printf("\n");
+}
 
+//37. 求3-1000孪生素数（相邻的两个素数）
+void c37() {
+	int n, i, index;
+	int a[maxsize], k = 0;
+	for (n = 3; n <= 1000; n++) {
+		index = 1;
+		//判断素数
+		for (i = 2; i < n; i++) {
+			if (n % i == 0)
+				index = 0;
+		}
+		if (index == 1) {
+			a[k] = n;
+			k++;
+		}
+	}
+	for (i = 0; i <= k; i++) {
+		if (a[i] - a[i + 1] == 2) {
+			printf("%d 和 %d 是孪生素数\n", a[i], a[i + 1]);
+		}
+	}
+}
 //38. 求回文数
+void c38() {
+	struct LNode* st;
+	struct LNode* p;
+	struct LNode* p1;
+	st = (LNode*)malloc(sizeof(LNode));
+	st->next = NULL;
+	int n, i, m, j, x;
+	int s = 0, state = 1;
+	for (n = 0; n < 256; n++) {
+		state = 1;
+		m = n * n;
+		//将每一位数存于栈内，并且求该数是几位数
+		while ((m / 10 != 0) ) {
+			p = (LNode*)malloc(sizeof(LNode));
+			p->data = m % 10;
+			p->next = st->next;
+			st->next = p;
+			m = m / 10;
+			s++;
+		}
+		m = n * n;
+		printf("%d 是 %d 位数\n ", n, s);
+		for (j = s; j > s / 2; j--) {
+			//出栈
+			p = st->next;
+			x = st->data;
+			if (x != (m % 10)) {
+				state = 0;
+			}
+			else {
+				st->next = p->next;
+				m = m / 10;
+			}
+		}
+		if (state != 0)
+			printf("%d \n", n);
+	}
+}
 
 //39. 判断一个五位数是否是回文数
+void c38() {
+	int n, i;
+	printf("请输入一个五位数：\n");
+	scanf_s("%d", &n); 
+	if ((n % 10 == n / 10000) && (n / 10 % 10 == n / 1000 % 10)) {
+		printf("%d 是回文数\n",n);
+	}
+}
 
 //40. 求100以内的所有勾股数
+void c40() {
+	int i, j, k;
+	int n;
+	for (i = 1; i < 100; i++) {
+		for (j = i; i < 100; i++) {
+			for (k = j; k < 100; k++) {
+				if ((j + k < i) || (i+j < k) || (j + k < i)) {
+					//两边之和小于第三边
+					break;
+				}
+				else {
+					if (i * i == (j * j + k * k)) {
+						printf("%d %d %d是勾股数\n", i, j, k);
+					}
+				}
+			}
+		}
+	}
+}
 
 //41. 输入一个字符串，判断其是否为回文
+void c41() {
+	char ch[maxsize];
+	char c;
+	int i, j, k, index = 1;
+	k = 0;
+	while ((c = getchar()) != '\n') {
+		ch[k] = c;
+		k++;
+	}
+	for (i = 0, j = (k - 1); i<k / 2, j>k / 2; i++, j--) {
+		index = 1;
+		if (ch[i] != ch[j]) {
+			index = 0;
+			printf("不是回文\n");
+			break;
+		}
+	}
+	if (index == 1) {
+		printf("是回文\n");
+	}
+}
 
 //42. 删除字符串中间的*号
+void c42() {
+	char ch[maxsize];
+	char c;
+	int i = 0, j, k;
+	printf("请输入字符串：\n");
+	while ((c = getchar()) != '\n') {
+		ch[i] = c;
+		i++;
+	}
+	int len = i;
+	for (j = 1; j < i - 1; j++) {
+		if (ch[j] == '*') {
+			if (ch[j - 1] > 'A' && ch[j - 1] < 'Z') {
+				if (ch[j + 1] > 'A' && ch[j + 1] < 'Z') {
+					//左移
+					for (k = j; k < i-1; k++) {
+						ch[k] = ch[k + 1];
+						len--;
+					}
+				}
+			}
+		}
+	} 
+	for (i = 0; i < len; i++)
+		printf("%c ", ch[i]);
+	printf("\n");
+}
 
 //43. 实现函数strlen
+int c43(char *string) {
+	int i = 0;
+	int length = 0;
+	while (string[i] != '/0') {
+		length++;
+		i++;
+	}
+	return length;
+}
 
 //44. 实现函数atoi
+int c44(char* s) {
+	int i = 1, n;
+	while (s[i] != '/0') {
+		n += (s[i] - '0') * i;
+		i *= 10;
+	}
+	printf("result = %d \n", n);
+	return n;
+}
 
 //45. 实现函数itoa
+void c45(int n, char* s) {
+	int i = 0;
+	int k = 1;
+	while (n / 10 != 0) {
+		s[i] = (n % 10) + '0';
+		n = n / 10;
+		i++;
+	}
+	for (k = 0; k < i; k++) {
+		printf("%c ", s[k]);
+	}
+	printf("\n");
+}
 
 //46. 实现函数strcat
+char* c46(char* strDes, const char* strSrc) {
+	int i = 0, j = 0, k;
+	while (strDes[i] != '/0')
+		i++;
+	while (strSrc[j] != '/0') {
+		strDes[i] = strSrc[j];
+		j++;
+		i++;
+	}
+	for (k = 0; k < i; k++) {
+		printf("%c ", strDes[k]);
+	}
+	printf("\n");
+	return strDes;
+}
 
 //47. 实现函数strncat
+char* c47(char* strDes, const char* strSrc, int count) {
+	int i = 0, j = 0, k;
+	while (strDes[i] != '/0')
+		i++;
+	while (count != 0) {
+		strDes[i] = strSrc[j];
+		i++; 
+		j++;
+	}
+	for (k = 0; k < i; k++) {
+		printf("%c ", strDes[k]);
+	}
+	printf("\n");
+	return strDes;
+}
 
 //48. 实现函数strcpy
+char* c48(char* strDes, const char* strSrc) {
+	int i = 0, j = 0, k;
+	while (strSrc[i] != '/0') {
+		strDes[j] = strSrc[j];
+		i++;
+		j++;
+	}
+	for (k = 0; k < i; k++) {
+		printf("%c ", strDes[k]);
+	}
+	printf("\n");
+	return strDes;
+}
 
 //49. 实现函数strncpy
+char* c49(char* strDes, const char* strSrc, int count) {
+	int i = 0, j = 0, k;
+	while (count != 0) {
+		strDes[i] = strSrc[j];
+		i++;
+		j++;
+	}
+	for (k = 0; k < i; k++) {
+		printf("%c ", strDes[k]);
+	}
+	printf("\n");
+	return strDes;
+}
 
 //50. 实现函数strcmp
+int c50(const char* s, const char* t) {
+	int i = 0, j = 0, k;
+	while (s[i] != '/0' || t[j] != '/0') {
+		if (s[i] != t[j]) {
+			printf("两字符串不等\n");
+			return 0;
+		}
+		i++;
+		j++;
+	}
+	if (s[i] != '/0' || t[j] != '/0') {
+		printf("两字符串不等\n");
+		return 0;
+	}
+	printf("两字符串相等\n");
+	return 1;
+}
 
 //51. 实现函数strncmp
+int strncmp(const char* s, const char* t, int count) {
+	int i = 0, j = 0, k;
+	while ((s[i] != '/0' || t[j] != '/0') || count != 0) {
+		if (s[i] != t[j]) {
+			printf("两字符串不等\n");
+			return 0;
+		}
+		i++;
+		j++;
+		count--;
+	} 
+	printf("两字符串相等\n");
+	return 1;
+}
+
+//52. 利用梯形法计算定积分f(a->b)(x^3+3*x^2-x+2)dx
+void c52(int a,int b) {
+	float n, r;
+	int i;
+	n = (b - a) / ;
+}
+
+//53. 编程计算空间上任意两点之间的距离
+void c53() {
+	struct Point p1 = {1,2,3};
+	struct Point p2 = { 3,4,5 };
+	int dis;
+	dis = sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y) + (p1.z - p2.z) * (p1.z - p2.z));
+	printf("distance = %d\n", dis);
+}
+
+//54. 日期运算问题
+typedef struct date {
+	int year;
+	int month;
+	int day;
+};
+void c54() {
+	//计算某一天是对应年的第几天
+	struct date date1 = {};
+	int sum = 0;
+	switch (date1.month) {
+		case 1:
+			sum = 0;
+			break;
+		case 2:
+			sum += 31;
+	}
+}
+
+//55. 统计各字符个数
+
+//56. 统计文件的字符数,单词数以及总行数
+
+//57. 统计英文字母，空格，数字和其它字符的数目
+
+//58. 大小写转换并输入到文件保存
+
+//59. 利用递归方法求5！
+
+//60. 输入三个字符串，将其按照从小到大排序并输出
+
 
 
 int main() { 
